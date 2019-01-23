cc73361e8fd3c2f7778d84b4471eb4d7bb1b9f5c
whylikethis <waylikethis@gmail.com>
2019-01-23 20:41:55 +0200

    Add files via upload

---
+++ task2_ItaiEinati-StavBarShay/Default.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+</head >
+<body dir="rtl" lang="he" style="font-family: Arial;">
+    <form id="form1" runat="server">
+        <div>
+            שם המשחק -
+            <asp:TextBox ID="gameName" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            גוף השאלה -&nbsp;&nbsp;
+            <asp:TextBox ID="Qtxt" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            תמונה כחלק מגוף השאלה -&nbsp;
+            <asp:TextBox ID="picQtxt" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            זמן מענה לשאלה -
+            <asp:TextBox ID="timeTxt" runat="server"></asp:TextBox>
+            <br />
+            <strong>
+            <br />
+            </strong>התשובה הנכונה היא - <span>
+                <asp:RadioButtonList ID="correctAnswer" runat="server" Width="822px" RepeatDirection="Horizontal" Style="display: inline-block; margin-top: 0px;">
+                    <asp:ListItem Value="0">אפשרות תשובה 1</asp:ListItem>
+                    <asp:ListItem Value="1">אפשרות תשובה 2</asp:ListItem>
+                    <asp:ListItem Value="2">אפשרות תשובה 3</asp:ListItem>
+                    <asp:ListItem Value="3">אפשרות תשובה 4</asp:ListItem>
+                </asp:RadioButtonList>
+            <br />
+            <br />
+            <br />
+            אפשרות תשובה 1 - <asp:TextBox ID="option0" runat="server"></asp:TextBox>
+            <br />
+            אפשרות תשובה 2 -
+            <asp:TextBox ID="option1" runat="server"></asp:TextBox>
+            <br />
+            אפשרות תשובה 3 -
+            <asp:TextBox ID="option2" runat="server"></asp:TextBox>
+            <br />
+            אפשרות תשובה 4 -
+            </span>
+            <asp:TextBox ID="option3" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="typeXML" runat="server" Text="הזן מידע לקובץ XML" OnClick="typeXML_Click" />
+            &nbsp;&nbsp;&nbsp;
+            <asp:Button ID="printXML" runat="server" Text="הדפס XML" OnClick="printXML_Click" />
+            &nbsp;
+            <br />
+            <br />
+
+
+            <asp:TextBox ID="myXmlText" runat="server" Height="315px" TextMode="MultiLine" Width="423px"></asp:TextBox>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ task2_ItaiEinati-StavBarShay/Default.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml; // שורה שחייבת להיכתב בתחילת מסמך על מנת לדבר עם קובץ האקסמאל
+using System.IO; // 
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void typeXML_Click(object sender, EventArgs e)
+    {
+        XmlTextWriter a = new XmlTextWriter(Server.MapPath("trees/game.xml"), System.Text.Encoding.UTF8);
+        a.WriteStartDocument();
+        a.WriteStartElement("game");
+        a.WriteAttributeString("name", (gameName.Text));
+        a.WriteAttributeString("time", (timeTxt.Text));
+        a.WriteStartElement("question");
+        a.WriteElementString("text", Qtxt.Text);
+        a.WriteElementString("picture", picQtxt.Text);
+        a.WriteStartElement("distractorsDetails");
+        string mySelect = correctAnswer.SelectedValue;
+        for (int i = 0; i < 4; i++)
+        {
+            if (mySelect != i.ToString())
+            {
+                mySelect = "inCorrect";
+            }
+            else
+            {
+                mySelect = "correct";
+            }
+            string textName = ((TextBox)FindControl("option" + i.ToString())).Text;
+            a.WriteStartElement("option");
+            a.WriteAttributeString("feedback", mySelect);
+            a.WriteElementString("text", textName);
+            a.WriteEndElement();
+        }
+
+        a.WriteEndElement();
+        a.WriteEndElement();
+        a.WriteEndElement();
+        a.WriteEndDocument();
+        a.Close();
+    }
+
+    protected void printXML_Click(object sender, EventArgs e)
+    {
+        MemoryStream ActivityStream = new MemoryStream();
+        XmlTextWriter a = new XmlTextWriter(ActivityStream, System.Text.Encoding.UTF8);
+        a.WriteStartDocument();
+        a.WriteStartElement("game");
+        a.WriteAttributeString("name", (gameName.Text));
+        a.WriteAttributeString("time", (timeTxt.Text));
+        a.WriteStartElement("question");
+        a.WriteElementString("text", Qtxt.Text);
+        a.WriteElementString("picture", picQtxt.Text);
+        a.WriteStartElement("distractorsDetails");
+        string mySelect = correctAnswer.SelectedValue;
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (mySelect != i.ToString())
+            {
+                mySelect = "inCorrect";
+            }
+            else
+            {
+                mySelect = "correct";
+            }
+            string textName = ((TextBox)FindControl("option" + i.ToString())).Text;
+            a.WriteStartElement("option");
+            a.WriteAttributeString("feedback", mySelect);
+            a.WriteElementString("text", textName);
+            a.WriteEndElement();
+        }
+
+        a.WriteEndElement();
+        a.WriteEndElement();
+        a.WriteEndElement();
+        a.WriteEndDocument();
+        a.Flush();
+        ActivityStream.Position = 0;
+        StreamReader reader = new StreamReader(ActivityStream);
+        myXmlText.Text = reader.ReadToEnd();
+        a.Close();
+    }
+}
